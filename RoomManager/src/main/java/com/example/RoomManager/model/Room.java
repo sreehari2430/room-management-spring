5133b0571c179aafe739ac4a0cf9ce9804dc9e4b
sreehari <harivinod22@gmail.com>
2025-10-17 13:13:00 +0530

    Add room type and price to model

--- RoomManager/src/main/java/com/example/RoomManager/model/Room.java
+++ RoomManager/src/main/java/com/example/RoomManager/model/Room.java
@@ -1,20 +1,34 @@
 package com.example.RoomManager.model;
 
+enum RoomType {
+      SINGLE,
+      DOUBLE,
+      SUITE
+}
+
 public class Room {
-    private int roomNumber;
-    private boolean booked;
+      private int roomNumber;
+      private RoomType roomType;
+      private double price;
+      private boolean booked;
 
-    public Room(int roomNumber) {
-        this.roomNumber = roomNumber;
-        this.booked = false;
-    }
+      public Room(int roomNumber, RoomType roomType, double price) {
+            this.roomNumber = roomNumber;
+            this.roomType = roomType;
+            this.price = price;
+            this.booked = false;
+            }
 
-    public int getRoomNumber() { return roomNumber; }
-    public boolean isBooked() { return booked; }
-    public void setBooked(boolean booked) { this.booked = booked; }
+      public Room(int roomNumber) {
+            this.booked = false;
+            }
 
-    @Override
-    public String toString() {
-        return "Room{" + "roomNumber=" + roomNumber + ", booked=" + booked + '}';
-    }
+      public int getRoomNumber() { return roomNumber; }
+      public boolean isBooked() { return booked; }
+      public void setBooked(boolean booked) { this.booked = booked; }
+
+      @Override
+      public String toString() {
+            return "Room{" + "roomNumber=" + roomNumber + ", booked=" + booked + '}';
+      }
 }
